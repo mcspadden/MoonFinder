46564fdd363ffa5fa438f51d61225239189634c8
matthew <34407450+mcspadden@users.noreply.github.com>
2018-02-23 22:21:44 -0700

    Start of API calls for data
    Probably need to update the Python var(s) as they are not very well named

--- Moonfinder.py
+++ Moonfinder.py
@@ -1,9 +1,8 @@
 # McSpadden
-
+import time, requests
 
 print('MoonFinder by McSpadden')
 print('Please wait while exceed the escape velocity of Earth...')
-import time  # import time to make a mini delay
 time.sleep(1) # changes delay speed
 
 # Supported currencies
@@ -39,3 +38,25 @@ while ans: # line below print's the symbol for default currencies
         break
     else: # if input was not a supported currency
         print('Not a supported currency at this time. Please try another currency.')
+
+# Place below code in correct location later on
+coinA=input('What coin would you like to select?').upper()
+print('Please wait while your first class seat is being prepared.')
+# Request to get coin's history
+A1_Old = requests.get("http://coincap.io/history/LTC")
+# Place JSON data as python var
+A1 = A1_Old.json()
+# Select the coins price on day 365
+A1_365 = (A1["price"][0][1])
+# Coin A still: Request to get coins data for today
+A2_Now = requests.get("http://coincap.io/page/LTC")
+# Place JSON data as python var
+A2 = A2_Now.json()
+# Select the coins price in USD for today
+A2_Today= (A2["price_usd"])
+# Creates var for Coin A's price difference in a percentage
+print("Today: $" + str(A2_Today))
+print("365 Days Ago: $" + str(A1_365))# remove after testing is finished
+A_Difference = round((A2_Today - A1_365) / A1_365 * int("100")) # Forumla: Insert the formula here
+# Prints the percentage of gains for Coin A
+print(str(coinA) + " gains: " + str(A_Difference) + '%')
